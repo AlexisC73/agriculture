031fc0c8321163f4911ad729948dc95c153bbfe4
Alexis Comte <alexis.comte73@icloud.com>
2021-02-24 11:46:37 +0100

    leger fix stroke

--- Components/assets/icon/Apple.tsx
+++ Components/assets/icon/Apple.tsx
@@ -4,26 +4,26 @@ export default function Apple () {
         <svg width="100%" height="100%" viewBox="0 0 1064 1064" version="1.1">
             <g transform="matrix(1,0,0,1,-301.396,-301.396)">
                 <g transform="matrix(0.868564,0,0,0.868564,92.6691,162.26)">
-                    <circle cx="852.746" cy="772.624" r="604.038" fill='white' stroke='currentColor' strokeWidth='16px'/>
+                    <circle cx="852.746" cy="772.624" r="604.038" fill='white' stroke='currentColor' strokeWidth='20px'/>
                 </g>
                 <g id="Pomme" transform="matrix(1,0,0,1,-8.89329,178.37)">
                     <path
                         d="M835.44,453.013C835.44,453.013 782.976,408.532 673.008,436.554C563.04,464.576 375.395,627.543 587.862,986.454C587.862,986.454 666.985,1140.17 833.333,1011.34C833.333,1011.34 929.336,1125.94 1048.58,1022.19C1167.82,918.446 1320.86,515.62 1010.49,428.695C1010.49,428.695 925.68,387.191 835.44,453.013Z"
-                        fill='none' stroke='currentColor' strokeWidth='7px'/>
+                        fill='none' stroke='currentColor' strokeWidth='15px'/>
                     <g transform="matrix(0.956419,0.291998,-0.321501,1.05305,274.412,-255.283)">
                         <path d="M568.293,548.288C568.293,548.288 487.159,665.071 556.128,783.463" fill='none'
-                              stroke='currentColor' strokeWidth='6px'/>
+                              stroke='currentColor' strokeWidth='10px'/>
                     </g>
                     <g transform="matrix(0.959793,0.280709,-0.280709,0.959793,239.736,-185.053)">
                         <path d="M579.917,821.885C579.917,821.885 588.252,846.598 607.945,854.46" fill='none'
-                              stroke='currentColor' strokeWidth='6px'/>
+                              stroke='currentColor' strokeWidth='10px'/>
                     </g>
                     <g transform="matrix(0.483017,0.135204,-0.135204,0.483017,559.002,-265.438)">
                         <path
                             d="M906.46,1132.82C869.358,1084.81 892.238,752.287 1157.18,738.871C1216.65,1007.91 1002.86,1116.07 906.46,1132.82Z"
-                            fill='none' stroke='currentColor' strokeWidth='15px'/>
+                            fill='none' stroke='currentColor' strokeWidth='25px'/>
                     </g>
-                    <path d="M842.227,404.888L958.117,292.52" fill='none' stroke='currentColor' strokeWidth='7px'/>
+                    <path d="M842.227,404.888L958.117,292.52" fill='none' stroke='currentColor' strokeWidth='13px'/>
                 </g>
             </g>
         </svg>
