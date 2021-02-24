32c9eec3e26ec5def48e229ae9e030f7ab09b5cb
Alexis Comte <alexis.comte73@icloud.com>
2021-02-24 10:32:22 +0100

    ajout fleche + fin banner mobile

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -1,4 +1,5 @@
 import style from '../styles/Hero.module.scss'
+import ArrowDown from "./assets/icon/ArrowDown";
 
 export default function Hero() {
     return (
@@ -6,6 +7,9 @@ export default function Hero() {
             <div className={style.overlay}></div>
             <h1>Mieux Manger</h1>
             <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
+            <div className={style.arrowDown}>
+                <ArrowDown />
+            </div>
         </div>
     )
 }
