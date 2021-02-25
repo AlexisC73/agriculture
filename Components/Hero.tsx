ddd4ecc79bfc90b3e83b8b3ba6f977287960d326
Alexis Comte <alexis.comte73@icloud.com>
2021-02-25 11:08:21 +0100

    fin partie reco + ajout anim

--- Components/Hero.tsx
+++ Components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
             <h1>Mieux Manger</h1>
             <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</p>
             <div className={style.arrowDown}>
-                <ArrowDown />
+                <a href='#recommandations'><ArrowDown /></a>
             </div>
         </div>
     )
--- Components/Recommandations.tsx
+++ Components/Recommandations.tsx
@@ -1,14 +1,40 @@
 import style from '../styles/Recommandations.module.scss'
 import Ble from "./assets/icon/ble";
 import Apple from "./assets/icon/Apple";
+import {useLayoutEffect, useRef} from 'react'
 
 export default function Recommandations () {
+    const listRef = useRef()
+    useLayoutEffect(() => {
+        if (listRef['current'] !== undefined) {
+            // @ts-ignore
+            const children = [...listRef.current.childNodes]
+            const observer = new IntersectionObserver(function (entries) {
+                entries.forEach(entry => {
+                    if(entry.intersectionRatio >= 0.5) {
+                        entry.target.classList.remove(style.not_visible)
+                        observer.unobserve(entry.target)
+                    }
+                })
+            }, {
+                root: null,
+                threshold: [0.5]
+            })
+
+
+            children.forEach(child => {
+                child.classList.add(style.not_visible)
+                observer.observe(child)
+            })
+        }
+    })
+
     return (
-        <section className={style.recommandations}>
+        <section id='recommandations' className={style.recommandations}>
             <h2>Nos engagements</h2>
-            <ul>
+            <ul ref={listRef}>
                 <li><Ble /> <p>Une agriculture raisonnée</p></li>
-                <li><p>Une agriculture raisonnée</p><Apple /></li>
+                <li><Apple /><p>Des produits de saison</p></li>
                 <li><Ble /> <p>Une agriculture raisonnée</p></li>
             </ul>
         </section>
