af57ab60ca0eebd9b1a17b7d4e859ec99e883893
Alexis Comte <alexis.comte73@icloud.com>
2021-02-26 10:09:30 +0100

    ajout meta description

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Header from '../Components/Header'
 import Hero from '../Components/Hero'
-import MangezLocal from '../Components/MangezLocal'
 // @ts-ignore
 import Recommandations from '../Components/Recommandations';
+import MangezLocal from '../Components/MangezLocal'
 
 export default function Home() {
     return (
@@ -11,6 +11,7 @@ export default function Home() {
             <Head>
                 <title>Accueil</title>
                 <link rel="icon" href="/favicon.ico" />
+                <meta name="description" content="Vous souhaitez manger mieux tout en achetant des produits locaux aux meilleurs prix ?" />
             </Head>
 
             <Header />
